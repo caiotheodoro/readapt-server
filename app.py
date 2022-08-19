deeb3ffa4419613b02993ded5e2d5fb7991b57bf
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-08-19 02:03:11 -0300

    env port

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
-from flask_cors import CORS, cross_origin   # cors para chamadas de outros domínios
+from concurrent.futures import process
+# cors para chamadas de outros domínios
+from flask_cors import CORS, cross_origin
 from flask import Flask, request       # flask para criar a aplicação
 import os                          # os para acesso aos arquivos
 from predict import predict_age    # função do openCV
 from spark import filterByClassification    # spark para a filtragem
-app = Flask(__name__)          # cria a aplicação   
-UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')    # pasta de uploads
-CORS(app)                     
+app = Flask(__name__)          # cria a aplicação
+UPLOAD_FOLDER = os.path.join(os.path.dirname(
+    __file__), 'uploads')    # pasta de uploads
+CORS(app)
+
+
 @app.route("/")                                          # rota raiz
 def hello():
     return "Hello World!"
 
+
 @app.route("/upload", methods=['POST'])                # rota de upload
-@cross_origin(origin='localhost', headers=['Content- Type',   'Authorization']) 
+@cross_origin(origin='localhost', headers=['Content- Type',   'Authorization'])
 def upload_image():                                    # função de upload
     file = request.files['image']                    # pega o arquivo
-    save_path = os.path.join(UPLOAD_FOLDER, file.filename)  # caminho do arquivo
+    save_path = os.path.join(
+        UPLOAD_FOLDER, file.filename)  # caminho do arquivo
     file.save(save_path)                        # salva o arquivo
     try:                                    # tenta executar a função
-        age = predict_age(save_path)    # chama a função do openCV para predizer a idade
-        classification = filterByClassification(age)    # chama a função do spark com a idade
+        # chama a função do openCV para predizer a idade
+        age = predict_age(save_path)
+        # chama a função do spark com a idade
+        classification = filterByClassification(age)
         jsonFormatted = {                             # formata o json
             'age': age,
             'books': classification.collect()
@@ -26,10 +35,12 @@ def upload_image():                                    # função de upload
 
         return jsonFormatted                     # retorna o json
     except Exception as e:
-        print(e)                           # caso ocorra algum erro, imprime o erro
+        # caso ocorra algum erro, imprime o erro
+        print(e)
         age = "Error"
     return age
 
 
 if __name__ == "__main__":
-    app.run(debug=True)                        # executa o app
+    app.run(host='0.0.0.0', port=process.env.PORT |
+            5000)                  # executa o app
